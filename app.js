8ca1eb78248f7fbd06b5d3fd30bcb5b9b344928f
motd18 <96405462+motd18@users.noreply.github.com>
2022-01-10 14:00:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const musicianRoutes = require('./routes/musician');
 const app = express();
 const port = process.env.PORT || 3001;
 
+
+jshsjs
 // include routes
 app.use('/musician', musicianRoutes);
 
